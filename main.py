7ad19451a2e92bf05d7704189932ba48c8980cfd
porgetit <kevin.esguerra@utp.edu.co>
2025-04-05 21:31:47 -0500

    0.4.1

--- main.py
+++ main.py
@@ -48,6 +48,7 @@ if __name__ == "__main__":
 """
 BUG: El color del fondo no se tiene en cuenta al guardar la imagen.
 BUG: El trazado sobrepasa el área del lienza y termina pintando sobre la barra de herramientas.
+BUG: Al guardar, las curvas y los circulos no conservan información del algoritmo de dibujo y el grosor.
 TODO: Mejorar la interfaz de la barra de herramientas para que sea más atractiva y fácil de usar.
 TODO: Implementar la herramienta de dibujo a mano alzada. Teniendo en cuenta el grosor del pincel y el color.
 TODO: Implementar la herramienta de borrado a mano alzada. Teniendo en cuenta el grosor.
